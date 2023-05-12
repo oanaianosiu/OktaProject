aac3b5626de943c3f66a73c473f6e01b8290caa0
Parvan Andrei <andrei.parvan81@gmail.com>
2023-05-12 14:12:00 +0300

    final push before change from vue3 to vu2

--- okta-project/src/App.vue
+++ okta-project/src/App.vue
@@ -1,30 +1,64 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <nav class="green">
+      <div class="nav-wrapper">
+        <span class="brand-logo">
+          VueStacks Okta Demo
+        </span>
+        <ul class="right">
+          <li><a @click.prevent="login" v-if="!activeUser">Login</a></li>
+          <li><a @click.prevent="logout" v-if="activeUser">Logout</a></li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
 </template>
+
+<script>
+  export default {
+    name: 'App',
+    data () {
+      return {
+        activeUser: null
+      }
+    },
+    async created () {
+      await this.refreshActiveUser()
+    },
+    watch: {
+      '$route': 'refreshActiveUser'
+    },
+    methods: {
+      login() {
+        this.$auth.signInWithRedirect()
+      },
+      async refreshActiveUser () {
+        this.activeUser = await this.$auth.getUser()
+      },
+      async logout () {
+        await this.$auth.logout()
+        await this.refreshActiveUser()
+        this.$router.push('/loggedout')
+      }
+    }
+  }
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  margin-top: 65px;
 }
 
-nav {
-  padding: 30px;
+body {
+  background-color: #EAEDED;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.custom-contain {
+  margin-top: 100px;
 }
 </style>
--- okta-project/src/router/index.js
+++ okta-project/src/router/index.js
@@ -1,25 +1,46 @@
+import Vue from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { LoginCallback, navigationGuard } from '@okta/okta-vue'
+import LoggedOutComponent from '@/components/LoggedOut'
+import LoggedInComponent from '@/components/LoggedIn'
+import Auth from '@okta/okta-vue'
+
+
+
+Vue.use(Auth, {
+  issuer: 'https://dev-96776781.okta.com/oauth2/default',
+  client_id: '0oa7v0bmkvqE2mjzF5d7s',
+  redirect_uri: 'http://localhost:8080/login/callback',
+  scope: 'openid profile email app-role'
+})
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/loggedOut',
+    name: 'LoggedOut',
+    component: LoggedOutComponent
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/login/callback',
+    component: LoginCallback
+  },
+  {
+    path: '/',
+    name: 'LoggedIn',
+    component: LoggedInComponent,
+    meta: {
+      requiresAuth: true
+    }
+  },
+
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+
+router.beforeEach(navigationGuard)
 
 export default router
