520d3abffdd25d139c3a4568b88a26cf750dbe48
Parvan Andrei <andrei.parvan81@gmail.com>
2023-05-14 14:14:37 +0300

    added logo and home button

--- okta-project/src/App.vue
+++ okta-project/src/App.vue
@@ -2,47 +2,51 @@
   <div>
     <nav class="green">
       <div class="nav-wrapper">
-        <span class="brand-logo">
-          VueStacks Okta Demo
-        </span>
+        <router-link to="/loggedOut" class="header item" style="display: flex; align-items: center;">
+          <img class="ui mini image" src="./assets/logo.png" style="width: 60px; height: 60px;">
+          <span style="font-size: 20px; margin-left: 10px;">Baton Fagaras</span>
+        </router-link>
         <ul class="right">
           <li><a @click.prevent="login" v-if="!activeUser">Login</a></li>
-          <li><a @click.prevent="logout" v-if="activeUser">Logout</a></li>
+          <li><a @click.prevent="logout" v-if="activeUser" style="background-color: green;">Logout</a></li>
         </ul>
       </div>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    data () {
-      return {
-        activeUser: null
-      }
+export default {
+  name: 'App',
+  data() {
+    return {
+      activeUser: null
+    }
+  },
+  async created() {
+    await this.refreshActiveUser()
+  },
+  watch: {
+    '$route': 'refreshActiveUser'
+  },
+  methods: {
+    login() {
+      this.$auth.signInWithRedirect()
     },
-    async created () {
-      await this.refreshActiveUser()
+    async refreshActiveUser() {
+      this.activeUser = await this.$auth.getUser()
     },
-    watch: {
-      '$route': 'refreshActiveUser'
+    async logout() {
+      await this.$auth.signOut()
+      // await this.refreshActiveUser()
+      // this.$router.push('/loggedout')
     },
-    methods: {
-      login() {
-        this.$auth.signInWithRedirect()
-      },
-      async refreshActiveUser () {
-        this.activeUser = await this.$auth.getUser()
-      },
-      async logout () {
-        await this.$auth.logout()
-        await this.refreshActiveUser()
-        this.$router.push('/loggedout')
-      }
+    home(endpoint) {
+      this.$router.push(endpoint);
     }
   }
+}
 </script>
 
 <style>
